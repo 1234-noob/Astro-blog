79ec94dd3ea1334bc48a2bc2ac6f25bb6fc1eb2b
Sakshi Ambikar <chinmay22vinarkar@gmail.com>
2024-09-23 14:32:22 +0530

    final

---
+++ src/constant.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE:string= "TechPeople Blog";
+export const SITE_DESCRIPTION:string= "Articles, Stories & Tutorials for Tech People";
+export const HOMEPAGE_ARTICLE_LIMIT:number = 7;
+export const ARTICLES_PER_PAGE:number = 6;
+
